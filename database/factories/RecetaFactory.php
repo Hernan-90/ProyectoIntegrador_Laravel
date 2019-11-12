9e095b93844ea246f715642574360869bc0bfff6
JelyLezama <jelylezama@gmail.com>
2019-11-12 11:26:50 -0300

    controllers y models

---
+++ database/factories/RecetaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Receta;
+use Faker\Generator as Faker;
+
+$factory->define(Receta::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/UsuarioFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Usuario;
+use Faker\Generator as Faker;
+
+$factory->define(Usuario::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
