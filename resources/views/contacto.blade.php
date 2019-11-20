ac0127b9ad10072a4f2a1039942d9d5e43bbe183
Hernan <hernan.a.suasegu@gmail.com>
2019-11-20 13:22:09 -0300

    View de Contacto

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -1,53 +1,25 @@
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="widht=device-width,initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css">
-  <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Lato|Libre+Baskerville&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="/css/stylesheet.css">
-</head>
-  <body>
-    <header class="">
-        <div class="menu">
-          <ul>
-            <li>
-              <a href="/letisbakery">HOME</a>
-            </li>
-            <li>
-              <a href="/letisbakery/lacocina">LA COCINA</a>
-            </li>
-            <li>
-              <a href="/letisbakery/contacto">CONTACTO</a>
-            </li>
-            <li>
-              <a href="/letisbakery/cuentas">CUENTAS</a>
-            </li>
-            <!-- <li>
-              <a href="#">CHEF's</a>
-            </li> -->
-          </ul>
-        </div>
-        <div class="logo">
-          <a href="/letisbakery">
-            <img src="/images/Products/boceto.png" alt="">
-          </a>
-        </div>
-        <div class="redes">
-            <ul>
-              <li>
-                <a id="tw" href="#"><i class="fab fa-twitter"></i></a>
-              </li>
-              <li>
-                <a id="fb" href="#"><i class="fab fa-facebook-f"></i></a>
-              </li>
-              <li>
-                <a id="insta" href="#"><i class="fab fa-instagram"></i></a>
-              </li>
-            </ul>
-        </div>
-    </header>
-  <body>
-    
-  </body>
-</html>
+@extends('plantilla')
+
+@section('cuerpo')
+  <div class="cuerpo">
+    <h1>Sobre Nosotros</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <section>
+      <a href="#">telefono</a>
+      <ul>
+        <li>
+          <a href="#"></a>
+        </li>
+        <li>
+          <a href="#"></a>
+        </li>
+        <li>
+          <a href="#"></a>
+        </li>
+      </ul>
+    </section>
+    <aside class="">
+
+    </aside>
+  </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@
 // });
 
 Auth::routes();
-// Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get("/", "HomeController@index");
 
 Route::get("/cuentas", "CuentasController@index") ;
