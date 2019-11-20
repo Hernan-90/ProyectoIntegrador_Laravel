4d5c545ca9122500cf9ddabfc268aca6a740d3d6
Ana Gimenez <anagimenez@Ricardos-MBP-2.fibertel.com.ar>
2019-11-20 12:36:40 -0300

    fotos coci cocina

--- resources/views/lacocina.blade.php
+++ resources/views/lacocina.blade.php
@@ -1,93 +1,405 @@
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="widht=device-width,initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css">
-  <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Lato|Libre+Baskerville&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="/css/stylesheet.css">
-</head>
-  <body>
-    <header class="">
-        <div class="menu">
-          <ul>
-            <li>
-              <a href="/letisbakery">HOME</a>
-            </li>
-            <li>
-              <a href="/letisbakery/lacocina">LA COCINA</a>
-            </li>
-            <li>
-              <a href="/letisbakery/contacto">CONTACTO</a>
-            </li>
-            <li>
-              <a href="/letisbakery/cuentas">CUENTAS</a>
-            </li>
-            <!-- <li>
-              <a href="#">CHEF's</a>
-            </li> -->
-          </ul>
-        </div>
-        <div class="logo">
-          <a href="/letisbakery">
-            <img src="/images/Products/boceto.png" alt="">
-          </a>
-        </div>
-        <div class="redes">
-            <ul>
-              <li>
-                <a id="tw" href="#"><i class="fab fa-twitter"></i></a>
-              </li>
-              <li>
-                <a id="fb" href="#"><i class="fab fa-facebook-f"></i></a>
-              </li>
-              <li>
-                <a id="insta" href="#"><i class="fab fa-instagram"></i></a>
-              </li>
-            </ul>
-        </div>
-    </header>
-     <!-- Productos destacados -->
+@extends('plantilla')
+
+@section('cuerpo')
+     <!-- Productos destacados
       <div class="featured">
         <h1>PRODUCTOS DESTACADOS</h1>
-      </div>
-      <section class="featured-products"> -->
-        <h1>PRODUCTOS DESTACADOS</h1> -->
+      </div>-->
+      <section class="featured-products">
+        <h1>Productos Destacados</h1>
         <article class="product">
           <div class="photo-container">
-            <img class="photo" src="img/tortadenuez.jpg" alt="producto 1">
+            <section class="image">
+              <img src="/images/Products/tortadefrutillas.jpg" alt="">
+            </section>
           </div>
-          <h2 class="home">Torta de Nuez</h2>
+          <!--<h2 class="home">Torta de Frutillas</h2>-->
         </article>
         <div class="mid">
           <article class="product2">
             <div class="photo-containera">
-              <img class="photo2" src="img/pandeajo.jpg" alt="producto 2">
+              <section class="image">
+                <img src="/images/Products/pandeajo.jpg" alt="">
+              </section>
             </div>
-            <h2 class="home">Pan de Ajo y Parmesano</h2>
+               <!--<h2 class="home">Pan de Ajo y Parmesano</h2>-->
           </article>
           <article class="product2">
             <div class="photo-container">
-              <img class="photo2" src="img/sconesdequeso.jpg" alt="producto 3">
+              <section class="image">
+                <img src="/images/Products/sconesdequeso.jpg" alt="">
+              </section>
             </div>
-            <h2 class="home">Scones de Queso</h2>
+               <!--<h2 class="home">Scones de Queso</h2>-->
           </article>
         </div>
         <article class="product">
           <div class="photo-container">
-          <img class="photo3" src="img/alfajoresdemani.jpg" alt="producto 4">
+            <section class="image">
+              <img src="/images/Products/alfajoresdemani.jpg" alt="">
+            </section>
         </div>
-        <h2 class="home">Alfajores de Maní</h2>
+           <!--<h2 class="home">Alfajores de Maní</h2>-->
         </article>
-      </section> -->
- <!-- Footer -->
-      <!-- <footer>
-       <ul class="main-footer">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="lacocina.php">La cocina</a></li>
-        <li><a href="recetas.php">Recetas</a></li>
-        <li><a href="contacto.php">Contacto</a></li>
-      </ul>
-     </footer> -->
-  </body>
-</html>
+      </section>
+<section class="dulce">
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/brownie.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Brownie</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/lemonies.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Lemonies</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/muffinsarandanos.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Muffins de arandanos</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/muffinslimon.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Muffins de limon</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/cookiesavena.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Cookies de avena</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/cookieschocolate.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Cookies de chocolate</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/galletitasnaranja.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Galletitas de naranja</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/galletitaschips.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Galletitas con chips</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/alfajoresdemanteca.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Alfajorcitos de manteca</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/alfajoresdenuez.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Alfajorcitos de nuez</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/alfajoresdemani.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Alfajorcitos de mani</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/alfajoresdemaicena.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Alfajores de maicena</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/budinvainilla.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Budin de vainilla</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/budinbanana.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Budin de banana</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/budinlimon.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Budin de limon</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/budinnaranja.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Budin de naranja</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/budinmandarina.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Budin de mandarina</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/budinchocolate.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Budin de chocolate</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/grisines.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Grisines</h2>-->
+  </article>
+    </section>
+
+  <section class="salado">
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/sconesdequeso.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Scones de queso</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/galletitasdequeso.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Galletitas de queso</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/palitosqueso.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Palitos de queso</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/bizcochosgrasa.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Bizcochitos de grasa</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/focaccia.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Focaccia</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/panintegralsemillas.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Pan integral con semillas</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/pancampo.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Pan de campo</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/panfrances.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Pan frances</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/pansesamo.jpeg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Pan con semillas de sesamo</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/panchocolate.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Pan con chocolate</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/pancitosdulces.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Pancitos dulces</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/rogel.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Rogel</h2>-->
+  </article>
+  </section>
+
+  <section class="Tortas">
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/bizcochuelo.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Bizcochuelo</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/sableedealmendras.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Sablee de almendras</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/tartadecoco.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Tarta de coco</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/tartalima.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Tarta de lima</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/tartamanzana.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Tarta de manzana</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/tortachocolate.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Torta de chocolate</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/tortadefrutillas.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Torta de frutillas</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/tortahumeda.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Torta humeda de chocolate</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/havanet.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Havanet</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/havanetblanca.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Havanet con chocolate blanco</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/lemonpie.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Lemon pie</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/chocotorta.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Chocotorta</h2>-->
+  </article>
+  <article class="product">
+    <div class="photo-container">
+      <section class="image">
+        <img src="/images/Products/tortaoreo.jpg" alt="">
+      </section>
+    </div>
+    <!--<h2 class="home">Torta de oreo</h2>-->
+  </article>
+</section>
+@endsection
